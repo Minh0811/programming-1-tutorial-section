e3d407bf44aef9cc5399b9ed9fc4e053c38b065f
Minh <vokhaiminh0811@gmail.com>
2022-06-28 22:57:49 +0700

    Exercise 3 - 4

--- src/section1.java
+++ src/section1.java
@@ -1,7 +1,7 @@
 package src;
 
 import java.util.Scanner;
-
+import java.util.*;
 public class section1 {
     public static void main(String[] args){
         //Exercise 1
@@ -26,10 +26,25 @@ public class section1 {
         s = S % 3600 % 60;
 
         System.out.println(h + ":" + m + ":" + s);
+
         //Exercise 4
+        System.out.println("Input Ax: ");
+        double Ax = scanner.nextInt();
+        System.out.println("Input Ay: ");
+        double Ay = scanner.nextInt();
+        System.out.println("Input Bx: ");
+        double Bx = scanner.nextInt();
+        System.out.println("Input By: ");
+        double By = scanner.nextInt();
 
+        double AB = Math.pow(Ax - Bx, 2) + Math.pow(Ay - By, 2);
+        AB = Math.sqrt(AB);
 
+        double  circumference = AB * 4;
 
+        double area = Math.pow(AB, 2);
+
+        System.out.println(circumference + "And " + area);
         //Exercise 5
         //Exercise 6
         //Exercise 7
