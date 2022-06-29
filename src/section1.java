506bea574eb580cb405213f135ad340a522c2d16
Minh <vokhaiminh0811@gmail.com>
2022-06-29 23:42:12 +0700

    W2

--- src/section1.java
+++ src/section1.java
@@ -4,48 +4,51 @@ import java.util.Scanner;
 import java.util.*;
 public class section1 {
     public static void main(String[] args){
-        //Exercise 1
-        String myName = "Minh Vo";
-        System.out.println("***********");
-        System.out.println("* " + myName + " *");
-        System.out.println("***********");
-
-        //Exercise 2
-        int x = -10;
-        int y = -20;
-        int number = x;
-        x = y;
-        y = number;
-        System.out.println(x);
-        System.out.println(y);
-
-        //Exercise 3
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input your secound: ");
-        int S = scanner.nextInt();
 
-        int h = 0;
-        int m = 0;
-        int s = 0;
-
-        h = S / 3600;
-        m = S % 3600 / 60;
-        s = S % 3600 % 60;
-
-        System.out.println(h + ":" + m + ":" + s);
+        //Exercise 1
+//        String myName = "Minh Vo";
+//        System.out.println("***********");
+//        System.out.println("* " + myName + " *");
+//        System.out.println("***********");
+//
+//        //Exercise 2
+//        int x = -10;
+//        int y = -20;
+//        int number = x;6
+//        x = y;
+//        y = number;
+//        System.out.println(x);
+//        System.out.println(y);
+//
+//        //Exercise 3
+//        System.out.println("Input your secound: ");
+//        int S = scanner.nextInt();
+//
+//        int h = 0;
+//        int m = 0;
+//        int s = 0;
+//
+//        h = S / 3600;
+//        m = S % 3600 / 60;
+//        s = S % 3600 % 60;
+//
+//        System.out.println(h + ":" + m + ":" + s);
 
         //Exercise 4
         System.out.println("Input Ax: ");
-        double Ax = scanner.nextInt();
+        double Ax = scanner.nextDouble();
         System.out.println("Input Ay: ");
-        double Ay = scanner.nextInt();
+        double Ay = scanner.nextDouble();
         System.out.println("Input Bx: ");
-        double Bx = scanner.nextInt();
+        double Bx = scanner.nextDouble();
         System.out.println("Input By: ");
-        double By = scanner.nextInt();
+        double By = scanner.nextDouble();
 
         double AB = Math.pow(Ax - Bx, 2) + Math.pow(Ay - By, 2);
         AB = Math.sqrt(AB);
+
+        System.out.println(AB);
 
         double  circumference = AB * 4;
 
@@ -57,18 +60,18 @@ public class section1 {
         //Exercise 6
         //Exercise 7
         //Exercise 8
-        int n = 1;
-        while(n > 0){
-            System.out.print("Enter a positive interger: ");
-            n = scanner.nextInt();
-            if(n % 5 == 0 && n % 6 == 0 && n > 0){
-                System.out.println(n + " is divisible by both 5 and 6");
-            } else if (n % 5 == 0 && n > 0 || n % 6 == 0 && n > 0) {
-                System.out.println(n + " is divisible by both 5 or 6, but not both");
-            } else if (n % 5 != 0 && n % 6 != 0 && n > 0) {
-                System.out.println(n + " is neither divisible by both 5 nor 6");
-            }
-        }
-        System.out.println("Cya");
+//        int n = 1;
+//        while(n > 0){
+//            System.out.print("Enter a positive interger: ");
+//            n = scanner.nextInt();
+//            if(n % 5 == 0 && n % 6 == 0 && n > 0){
+//                System.out.println(n + " is divisible by both 5 and 6");
+//            } else if (n % 5 == 0 && n > 0 || n % 6 == 0 && n > 0) {
+//                System.out.println(n + " is divisible by both 5 or 6, but not both");
+//            } else if (n % 5 != 0 && n % 6 != 0 && n > 0) {
+//                System.out.println(n + " is neither divisible by both 5 nor 6");
+//            }
+//        }
+//        System.out.println("Cya");
     }
 }
